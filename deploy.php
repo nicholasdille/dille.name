8f5fbf992cbdfbb8a6aef84e013a22dfd500e6d1
Nicholas Dille <nicholas.dille@mailbox.org>
2019-08-30 11:49:03 +0200

    Updates

--- deploy.php
+++ deploy.php
@@ -4,20 +4,6 @@
         return $string;
     }
 
-    $message=shell_exec("mkdir -p ../www2 2>&1");
-    print_r(nl2br2($message));
-    $message=shell_exec("tar -xvzf ../site.tar.gz -C ../www2 --strip-components=1 2>&1");
-    print_r(nl2br2($message));
-
-    $message=shell_exec("rsync --verbose --recursive ./professional-powershell ../www2 2>&1");
-    print_r(nl2br2($message));
-    $message=shell_exec("rsync --verbose --recursive --delete-after ../www2/ ../www/ 2>&1");
-    print_r(nl2br2($message));
-
-    $message=shell_exec("rm -rfv ../www2/* ../www2/.htaccess 2>&1");
-    print_r(nl2br2($message));
-    $message=shell_exec("rmdir ../www2 2>&1");
-    print_r(nl2br2($message));
-    $message=shell_exec("rmdir ../site.tar.gz 2>&1");
+    $message=shell_exec("sh ./deploy.sh 2>&1");
     print_r(nl2br2($message));
 ?>
---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -o errexit
+
+echo "########## Check"
+sha256sum -c ../site.tar.gz.sha256
+
+echo "########## Unpacking"
+mkdir -p ../www2
+tar -xvzf ../site.tar.gz -C ../www2 --strip-components=1
+
+echo "########## Backing up professional-powershell"
+rsync --verbose --recursive ./professional-powershell ../www2
+
+echo "########## Syncing"
+rsync --verbose --recursive --delete-after ../www2/ ../www/
+
+echo "########## Cleaning up"
+rm -rfv ../www2/* ../www2/.htaccess
+rmdir ../www2
+rmdir ../site.tar.gz
