b16a2a8ec5917f219f67274f4ba881cdd30571a3
Nicholas Dille <nicholas.dille@mailbox.org>
2019-08-30 16:26:52 +0200

    Fixes for lunr 2

--- media/js/lunr-feed.js
+++ media/js/lunr-feed.js
@@ -2,21 +2,23 @@
 
 ---
 // collect documents
-var documents = [{% for post in site.posts %}{
+var data = [{% for post in site.posts %}{
+  "id": {{ forloop.index0 }}
   "title": {{ post.title | jsonify }},
   "link": {{ post.url | jsonify }},
   "date": {{ post.date | date: '%B %-d, %Y' | jsonify }},
   "excerpt": {{ post.excerpt | strip_html | jsonify }}
+  "tags": {{ post.tags | jsonify }}
 }{% unless forloop.last %},{% endunless %}{% endfor %}];
 
 // builds index
 var index = lunr(function () {
   this.field('title')
-  this.field('content', {boost: 10})
+  this.field('excerpt', {boost: 10})
   this.field('tags')
   this.ref('id')
 
-  documents.forEach(function (doc) {
+  data.forEach(function (doc) {
     this.add(doc)
   }, this)
 });
@@ -36,7 +38,7 @@ $(document).ready(function() {
     // Loop through, match, and add results
     for (var item in result) {
       var ref = result[item].ref;
-      var searchitem = '<div class="post"><h1 class="post-title"><a href="'+store[ref].link+'">'+store[ref].title+'</a></h1><span class="post-date">'+store[ref].date+'</span>'+store[ref].excerpt+'</div>';
+      var searchitem = '<div class="post"><h1 class="post-title"><a href="'+data[ref].link+'">'+data[ref].title+'</a></h1><span class="post-date">'+data[ref].date+'</span>'+data[ref].excerpt+'</div>';
       resultdiv.append(searchitem);
     }
   });
