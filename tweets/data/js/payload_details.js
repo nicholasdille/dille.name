218fd8f7dc2fa0558d85c37fa9f3b30eaed69ba4
Nicholas Dille <nicholas.dille@mailbox.org>
2024-02-19 15:20:59 +0100

    Removed outdated (and useless) content

--- tweets/data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details =  {
-  "tweets" : 2995,
-  "created_at" : "2018-03-03 13:41:04 +0000",
-  "lang" : "de"
-}
--- twitter/Twitter.ps1
+++
@@ -1,5 +0,0 @@
-﻿$Tweets = Get-Content -Path "$PSScriptRoot\tweets.csv" -Encoding Ascii `
-    | Select-Object -Skip 1 `
-    | ConvertFrom-Csv -Delimiter ',' -Header Id, InReplyToStatusId, InReplyToUserId, Timestamp, Source, Text, RetweetedStatusId, RetweetedStatusUserId, RetweetedStatusTimestamp, ExpandedUrls
-
-$Tweets[0]
