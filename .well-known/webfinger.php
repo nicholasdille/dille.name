cc5e552a393ed68284380e9501be2c3edf86c85c
seat1 <nicholas.dille@mailbox.org>
2024-10-24 10:38:57 +0200

    Added webfinger support

---
+++ .well-known/webfinger.php
@@ -0,0 +1,81 @@
+<?php
+$queries = array();
+parse_str($_SERVER['QUERY_STRING'], $queries);
+
+if (!array_key_exists('resource', $queries)) {
+    header("HTTP/1.1 400 Bad Request");
+    echo 'resource parameter missing';
+    exit();
+}
+if (count($queries) > 1) {
+    header("HTTP/1.1 400 Bad Request");
+    echo 'more parameters than resources found';
+    exit();
+}
+
+$accounts = array(
+    "acct:nicholas@dille.name" => array(
+        "subject" => "acct:nicholasdille@freiburg.social",
+        "aliases" => array(
+            "https://freiburg.social/@nicholasdille",
+            "https://freiburg.social/users/nicholasdille"
+        ),
+        "links" => array(
+            array(
+                "rel" => "http://webfinger.net/rel/profile-page",
+                "type" => "text/html",
+                "href" => "https://freiburg.social/@nicholasdille"
+            ),
+            array(
+                "ref" => "self",
+                "type" => "application/activity+json",
+                "href" => "https://freiburg.social/users/nicholasdille"
+            ),
+            array(
+                "rel" => "http://ostatus.org/schema/1.0/subscribe",
+                "template" => "https://freiburg.social/authorize_interaction?uri={uri}"
+            ),
+            array(
+                "rel" => 'self',
+                "type" => 'application/activity+json',
+                "href" => 'https://freiburg.social/users/nicholasdille'
+            ),
+            array(
+                "rel" => 'http://schemas.google.com/g/2010#updates-from',
+                "href" => 'https://freiburg.social/@nicholasdille',
+                "type" => 'application/activity+json',
+            ),
+            array(
+                "rel" => "http://webfinger.net/rel/avatar",
+                "type" => "image/jpeg",
+                "href" => "https://dille.name/nicholas.jpg"
+            ),
+            array(
+                "rel" => "http://packetizer.com/rel/businesscard",
+                "type" => "text/vcard",
+                "href" => "https://dille.name/nicholas.vcf"
+            ),
+            array(
+                "rel" => "http://packetizer.com/rel/blog",
+                "type" => "text/html",
+                "href" => "https://dille.name",
+                "titles" => array(
+                    "en-us" => "Automation, DevOps and containerization",
+                    "de-de" => "Automatisierung, DevOps und Containerisierung"
+                )
+            ),
+            array(
+                "rel" => 'alternate',
+                "type" => 'application/rss+xml',
+                "href" => 'https://blog.bajonczak.com/rss/',
+            )
+        )
+    )
+);
+
+header("Content-Type: application/json");
+if (!array_key_exists($queries['resource'], $accounts)) {
+    exit();
+}
+echo json_encode($accounts[$queries['resource']]);
+?>
